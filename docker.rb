90b4db3071fad11075145128f05715b3eaeca993
Aaron Miller <apage43@ninjawhale.com>
2014-01-29 14:24:03 -0800

    Bump docker to 0.7.6

--- docker.rb
+++ docker.rb
@@ -4,11 +4,11 @@ class Docker < Formula
   homepage 'https://www.docker.io/'
 
   if MacOS.prefer_64_bit?
-    url 'https://get.docker.io/builds/Darwin/x86_64/docker-0.7.4.tgz'
-    sha1 '4503c6f762cdc8310e3bb43babbb7f90b45697f7'
+    url 'https://get.docker.io/builds/Darwin/x86_64/docker-0.7.6.tgz'
+    sha1 '2f2e6815a8404623f3e31a8b146f9976b15d4117'
   else
-    url 'https://get.docker.io/builds/Darwin/i386/docker-0.7.4.tgz'
-    sha1 'b21bfc0eafddcadd40218c138ec4a4272adc7e93'
+    url 'https://get.docker.io/builds/Darwin/i386/docker-0.7.6.tgz'
+    sha1 'cf83bc667430f5e9e4c3241c73898348125b3b5a'
   end
 
   depends_on :arch => :intel
